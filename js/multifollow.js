549231a8889ca3ae17663746c3f29beebcf93efd
petr zaparka <netro82@gmail.com>
2009-11-04 22:55:17 +0100

    Replace tab space with normal space, for better formating.

--- js/multifollow.js
+++ js/multifollow.js
@@ -16,14 +16,14 @@ User.prototype = {
 }
 
 function TwitterManager() {
-	this.twitter_account = new Array();
+  this.twitter_account = new Array();
 };
 
 TwitterManager.prototype = {
   twitter_account: null,
-	twitter_login_state: 'unsigned',
-	name: null,
-	pass: null,
+  twitter_login_state: 'unsigned',
+  name: null,
+  pass: null,
   
   follow: function() {
     if( this.twitter_login_state == 'signed' ){
@@ -32,93 +32,89 @@ TwitterManager.prototype = {
         requested_follow_ids[ i ] = user.value;
       });
       var a = this;	
-  		this.ajax_call( 'PUT', '/', {
-  		      name: this.name,
-  		      password: this.password,
-  		      requested_follow_ids: requested_follow_ids.join(',') 
-  		    }, function( response ) {
-  		       $( "#users_list" ).empty();
-  		       $( '#message_box span' ).text( response );
-  		    }
-  		);
+      this.ajax_call( 'PUT', '/', {
+          name: this.name,
+          password: this.password,
+          requested_follow_ids: requested_follow_ids.join(',') 
+        }, function( response ) {
+           $( "#users_list" ).empty();
+           $( '#message_box span' ).text( response );
+        });
     }
   },
   
   search: function() {
     var target_account = $( '#target_account' ).val();
     if ( target_account == '' )
-			alert( 'You mast fill "twitter account source".' );
-		else {
-		  var a = this;	
-  		this.ajax_call( 'GET', '/' + target_account, '', function( response ) {
-  		       eval( response );
-  		       if ( response == 'false')
-  		          a.target_false();
-  		       else
-  		          a.update_users_list( users_list );
-  		    }
-  		);
-		} 
+      alert( 'You mast fill "twitter account source".' );
+    else {
+      var a = this;	
+      this.ajax_call( 'GET', '/' + target_account, '', function( response ) {
+           eval( response );
+           if ( response == 'false')
+              a.target_false();
+           else
+              a.update_users_list( users_list );
+      });
+    } 
   },
   
   update_users_list: function( users_list ) {
-	  $( '#message_box span' ).text( 'Twitter account source finded.' );
-	  $( "#users_list" ).empty().show();
+    $( '#message_box span' ).text( 'Twitter account source finded.' );
+    $( "#users_list" ).empty().show();
     jQuery.each( users_list, function( i, user ) {
       $( "#users_list" ).append( '<input name="friends" type="checkbox" value="' + user.id 
        + '"/><span>' + user.name + '</span><br/>' );
     });
-	},
-
-	target_false: function() {
-	  $( '#message_box span' ).text( 'Twitter account source finder failed.' );
-	},
-  
-  login: function() {
-		this.name = $( '#name' ).val();
-		this.password = $( '#pass' ).val();
-
-		if ( this.name == '' || this.password == '' )
-			alert( 'You mast fill name and login.' );
-
-		var a = this;	
-		this.ajax_call( 'POST', '/login', {
-		      name: this.name,
-		      password: this.password,
-		    }, function( response ) {
-		       eval( response );
-		       if ( response == 'false')
-		          a.login_false()
-		       else
-		          a.updateLoginState( client_data );
-		    }
-		);
   },
 
-	updateLoginState: function( client_data ) {
-	  $( '#message_box span' ).text( 'Login sucess.' );
-		$( '#login_form *' ).hide();
-		$( '#login_form' ).html( '<span> Welcome '+ client_data.name + '</span>' );
-		this.twitter_login_state = 'signed';
-	},
+  target_false: function() {
+    $( '#message_box span' ).text( 'Twitter account source finder failed.' );
+  },
+  
+  login: function() {
+    this.name = $( '#name' ).val();
+    this.password = $( '#pass' ).val();
 
-	login_false: function() {
-	  $( '#message_box span' ).text( 'Login failed, please try again.' );
-	},
+    if ( this.name == '' || this.password == '' )
+      alert( 'You mast fill name and login.' );
 
-	ajax_call: function( type, url, data, on_success_method ){
-	    $.ajax({
-	     type: type,
-	     url: url,
-	     dataType: "script",
-	     data: data,
-	     success: on_success_method,
-	     complete: function(){
-	       $( 'img.loading' ).hide();
-	       }
-	    });
-	  }
+    var a = this;	
+    this.ajax_call( 'POST', '/login', {
+          name: this.name,
+          password: this.password,
+        }, function( response ) {
+           eval( response );
+           if ( response == 'false')
+              a.login_false()
+           else
+              a.updateLoginState( client_data );
+    });
+  },
 
+  updateLoginState: function( client_data ) {
+    $( '#message_box span' ).text( 'Login sucess.' );
+    $( '#login_form *' ).hide();
+    $( '#login_form' ).html( '<span> Welcome '+ client_data.name + '</span>' );
+    this.twitter_login_state = 'signed';
+  },
+
+  login_false: function() {
+    $( '#message_box span' ).text( 'Login failed, please try again.' );
+  },
+
+  ajax_call: function( type, url, data, on_success_method ){
+    $.ajax({
+      type: type,
+      url: url,
+      dataType: "script",
+      data: data,
+      success: on_success_method,
+      complete: function(){
+        $( 'img.loading' ).hide();
+      }
+    });
+  }
 };
 
 $(document).ready(function () {
